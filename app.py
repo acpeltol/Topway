6a6f749c75beec55feea4aad2aa0b64bf5013ea4
acpeltol <apeltola717@gmial.com>
2024-03-25 13:10:54 +0200

    Second change with log in icon

--- app.py
+++ app.py
@@ -7,13 +7,24 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///acpeltol"
 db = SQLAlchemy(app) 
 
-@app.route("/")
+# Routes to different pages!
+
+
+#Main page
+
+@app.route("/", methods = ["POST", "GET"])
 def index():
+     
+    if request.method == "POST":
+        print(request.form["uname"])
+        print(request.form["upass"])
+
     print(db.session.execute(text('SELECT * FROM hulio')).fetchall())
     return render_template("base.html")
 
 @app.route("/page1")
 def page1():
+
     return render_template("register_page.html")
 
 @app.route("/page2")
