499aa4f5256aa3bd50ce4b103cf70837fc7fdf07
acpeltol <apeltola717@gmial.com>
2024-04-13 15:28:40 +0300

    Version in which I added password encryption and Session function to save user name. Also more html files for future and commenting on functions

--- app.py
+++ app.py
@@ -1,32 +1,50 @@
 from flask import Flask
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+
+app.secret_key = "Alejandro"
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///acpeltol"
-db = SQLAlchemy(app) 
+db = SQLAlchemy(app)
 
-# Routes to different pages!
+# Functions which checsk parameters and lead to right pages
 
-# Checks if new users username exists
+# Checks if new users username exists and if it has correct type password
 
 def new_user_check(uname, upass):
 
+    # If user types nothing then it rejects sign up
+    
     if uname == "" or upass == "":
         return "Wrog type of name or password"
     
-    u_id = db.session.execute(text(f'''SELECT id FROM users WHERE uname = '{uname}' ''')).fetchall()
+    # If password has not uppercase letters then it rejects password
+
+    if upass == upass.lower():
+        return "Password must have at least one capital letter"
     
-    print(u_id)
+    # If password is too short it rejects password
+
+    if len(upass) < 8:
+        return "Your password must be at least 8 charecters long"
+    
+    # If username alredy exists it rejects username
+
+    u_id = db.session.execute(text(f'''SELECT id FROM users WHERE uname = '{uname}' ''')).fetchall()
 
     if len(u_id) == 1:        
         return "Username alredy exists"
 
 
-    return "gato"
+    return 1
 
-#Main page
+#Routes to different pages
+
+#Opening page
 
 @app.route("/", methods = ["POST", "GET"])
 def index():
@@ -37,45 +55,61 @@ def index():
 
     if request.method == "POST":
 
-        u_id = db.session.execute(text(f'''SELECT id FROM users
-                                   WHERE uname = '{request.form["uname"]}' AND upass = '{request.form["upass"]}' ''')).fetchall()
+        u_id = db.session.execute(text(f'''SELECT id,upass  FROM users
+                                   WHERE uname = '{request.form["uname"]}' ''')).fetchall()
+
+        print(u_id)            
+
+        if len(u_id) != 1:            
             
-        #print(u_id)
-
-        #print(request.form["uname"])
-
-        #print(request.form["upass"])
-
-        if len(u_id) == 1:
-            return "Hello kioto"
+            texto = "Username is wrong"
+            return render_template("base.html", texto = texto)
     
-        else:
-            texto = "Username or paswors is wrong"
+        if check_password_hash(u_id[0][1], request.form["upass"]):
 
-    print(db.session.execute(text('SELECT * FROM hulio')).fetchall())
+            user = request.form["uname"]
+            session["user"] = user
+
+            return render_template("main_page.html", texto = session["user"])
+        
+        else:
+
+            texto = "Password is wrong"
+            return render_template("base.html", texto = texto)
+
     return render_template("base.html", texto = texto)
 
-# REGISTER PAGE
+# REGISRATION PAGE
 
 @app.route("/register", methods = ["POST", "GET"])
 def register():
 
     texto_register = ""
 
+    # If user writes down the name and the password it will go next statement
+
     if request.method == "POST":
         
         texto_register = new_user_check(request.form["runame"],request.form["rupass"])
 
-        if texto_register == "gato":
+        if texto_register == 1:
 
-            db.session.execute(text(f'''INSERT INTO users (uname, upass) VALUES ('{request.form["runame"]}', '{request.form["rupass"]}')'''))
+            db.session.execute(text(f'''INSERT INTO users (uname, upass) VALUES ('{request.form["runame"]}', '{generate_password_hash(request.form["rupass"])}')'''))
             db.session.commit()
 
-            print("hulio")
+            user = request.form["runame"]
+            session["user"] = user
+
+            return render_template("main_page.html",texto = session["user"])
+
 
 
     return render_template("register_page.html", texto_register = texto_register)
 
-@app.route("/page2")
-def page2():
-    return "Tämä on sivu 2"
+@app.route("/main_pgae", methods = ["POST", "GET"])
+def main_page():
+    return 
+
+@app.route("/get_information", methods = ["POST", "GET"])
+def get_information():
+    return render_template("register_page.html")
